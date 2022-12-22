d755f52b372eb174ef9e7621b23a0ce946a80cbb
tanish-sukhija <120703230+tanish-sukhija@users.noreply.github.com>
2022-12-22 21:42:28 +0530

    Add files via upload

---
+++ Q1.py
@@ -0,0 +1,5 @@
+a=int(input("Enter first number: "))
+b=int(input("Enter second number: "))
+c=int(input("Enter third number: "))
+d=(a+b+c)/3
+print("the average of a,b and c is "+ str(d))
---
+++ Q4.py
@@ -0,0 +1,4 @@
+num1=25
+num2='25'
+num3=25.0
+print(str(int(num1)+int(num2)+int(num3)))
